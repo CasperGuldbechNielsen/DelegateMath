0e19997c7c1f0927a2719dd74a7ae33d25417557
CasperGuldbechNielsen <casp674d@edu.easj.dk>
2016-02-24 11:00:58 +0100

    Working with delegates

---
+++ DelegateMath/DelegateMath.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace DelegateMath
+{
+    public class DelegateMath
+    {
+        public delegate double MyDelegate(double x);
+
+        public DelegateMath()
+        {
+            MyDelegate myDelegateSquare = new MyDelegate(Square);
+            MyDelegate myDelegateDoubleUp =  new MyDelegate(DoubleUp);
+
+            List<double> myDoubles = new List<double>( 
+                new double []
+                {
+                    10.5,
+                    1.6,
+                    2.3,
+                    3.78,
+                    2.8
+                });
+
+            foreach (var myDouble in myDoubles)
+            {
+                Display(myDelegateSquare, myDouble);
+            }
+
+            foreach (var myDouble in myDoubles)
+            {
+                Display(myDelegateDoubleUp, myDouble);
+            }
+        }
+
+        public double Square(double num)
+        {
+            num *= num;
+            return num;
+        }
+
+        public double DoubleUp(double num)
+        {
+            num *= 2;
+            return num;
+        }
+
+        public double Display(MyDelegate myDelegate, double num)
+        {
+            num = myDelegate(num);
+            Console.WriteLine(num);
+            return num;
+        }
+    }
+}
